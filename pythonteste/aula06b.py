197618a144de75f8d8056305364c8b9b9c2ab142
Pedro Rodrigues <pedroras004@gmail.com>
2021-09-07 12:15:10 -0300

    Completa aula 6b

--- pythonteste/aula06b.py
+++ pythonteste/aula06b.py
@@ -1,2 +1,10 @@
 n = input("Digite um valor: ")
-print(n.isupper())
+
+print('O tipo primitivo desse valor é ', type(n))
+print('Só tem espaços? ', n.isspace())
+print('É um número? ', n.isnumeric())
+print('É alfabético? ', n.isalpha())
+print('É alfanumérico? ', n.isalnum())
+print('Está em maiusculas? ', n.isupper())
+print('Está em minusculas? ', n.islower())
+print('Está capitalizada? ', n.istitle())
